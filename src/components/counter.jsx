2667a49563c15a3466673f8ea3cc963014ffe2af
Tate Harpster <tate.harpster@liferay.com>
2018-12-19 16:51:47 +0700

    added number incrementation on counter.jsx for score keeping WIP

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,31 +7,57 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
+    //You must use constructors if the " => " syntax breaks.
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = (player) => {
+        console.log(player);
+        this.setState({ count: this.state.count + 1 })
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p> There are no tags! </p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}</ul>;
+
+    };
+
     render() { 
         
         let classes = this.getBadgeClasses();
 
         return (
             <React.Fragment> 
-                <span className={classes}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-small"> Increment </button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
-                </ul>
+                <span 
+                    className={classes}>
+                    {this.formatCount()}
+                </span>
+                <button 
+                    onClick={(player) => this.handleIncrement(player)} 
+                    className="btn btn-secondary btn-small"> 
+                    Increment 
+                </button>
+
+                { this.state.tags.length === 0 && 'Please create a new tag.' }
+                {this.renderTags()}
             </React.Fragment>
         );
-    }
+    };
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
-    }
+    };
 
     formatCount(){
         const {count} = this.state;
         return count === 0 ? 'Zero': count;
-    }
+    };
 }
  
 export default Counter;
